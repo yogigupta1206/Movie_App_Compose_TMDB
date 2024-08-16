11f78a539abdbf08d184d23a6c739324ea4ad682
Yogesh Gupta <yogesh1.gupta@paytm.com>
2024-08-16 10:16:20 +0530

    Image Url Size Setup

--- app/src/main/java/com/yogigupta1206/movieapp/utils/Constants.kt
+++ app/src/main/java/com/yogigupta1206/movieapp/utils/Constants.kt
@@ -1,3 +1,6 @@
 package com.yogigupta1206.movieapp.utils
 
-const val BASE_URL = "http://www.test.com"
+const val BASE_URL = "https://api.themoviedb.org"
+const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+const val IMAGE_SIZE_W185 = "w185"
+const val IMAGE_SIZE_W780 = "w780"
